a7837852de584e24d96582421a2b8afa667081cf
Home <igor.kiyashko@inbox.ru>
2022-09-05 21:35:47 +0600

    Level 60,61

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -3,10 +3,13 @@ package com.zaurtregulov.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
     @RequestMapping("/")
     public String showFirstView(){
@@ -26,17 +29,26 @@ public class MyController {
 //    }
 
 
-    @RequestMapping("/showDetails")
-    public String showDetails(HttpServletRequest request, Model model)
-    {
-        String empName = request.getParameter("employeeName"); //employeeName берется из ссылки генерируемой в ask-emp-details-view
-        empName = "Mr. "+empName;
-        model.addAttribute("nameAttribute", empName);
+//    @RequestMapping("/showDetails")
+//    public String showDetails(HttpServletRequest request, Model model)
+//    {
+//        String empName = request.getParameter("employeeName"); //employeeName берется из ссылки генерируемой в ask-emp-details-view
+//        empName = "Mr. "+empName;
+//        model.addAttribute("nameAttribute", empName);
+//
+//        model.addAttribute("description", " - udemy instructor");
+//
+//
+//        return "show-emp-details-view";
+//    }
+@RequestMapping("/showDetails")
 
-        model.addAttribute("description", " - udemy instructor");
+public String showDetails(@RequestParam("employeeName") String empName , Model model)
+{
+    empName = "Mr. "+empName + "!";
+    model.addAttribute("nameAttribute", empName);
 
-
-        return "show-emp-details-view";
-    }
+    return "show-emp-details-view";
+}
 }
 
